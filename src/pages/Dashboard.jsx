53f96d559ecfe1de89605e60d986114ce039e840
Jitendra Gupta <jiitendra.gupta@outlook.com>
2026-09-18 12:26:44 +0530

    Fix typo dahboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard() {
   return (
     <>
       <Row type="horizontal">
-        <Heading as="h1">Dashboards</Heading>
+        <Heading as="h1">Dashboard</Heading>
         <DashboardFilter />
       </Row>
       <DashboardLayout />
